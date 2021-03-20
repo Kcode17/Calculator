8df51bde63a8e219ead6e920ebd858779697f4d5
Krishna Teja <78008005+Kcode17@users.noreply.github.com>
2021-03-20 19:17:22 -0400

    Feature: Added Square along with Unit Test

--- src/calculator.py
+++ src/calculator.py
@@ -25,3 +25,7 @@ class Calculator:
     def divide(self, a, b):
         self.result = division(a, b)
         return self.result
+
+    def square(self, a):
+        self.result = multiplication(a, a)
+        return self.result
