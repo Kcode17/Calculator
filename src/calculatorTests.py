ea13b6b243ec8af15427612679956c62bb67115f
Krishna Teja <78008005+Kcode17@users.noreply.github.com>
2021-03-20 18:25:53 -0400

    Feature: Addition Feature added with Test

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -3,10 +3,14 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_addition(self):
+        self.assertEqual(self.calculator.add(5, 5), 10)
 
 
 if __name__ == '__main__':
